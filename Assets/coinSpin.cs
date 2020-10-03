cc3265327bb96b9183f60fe19814625f87dd7b02
Jaishree Bala <jaishree1bala@gmail.com>
2020-10-03 01:34:24 -0400

    Stage 2: Coin Spin

---
+++ Assets/coinSpin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinSpin : MonoBehaviour
+{
+    public GameObject[] coins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coins = GameObject.FindGameObjectsWithTag("Coin");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject coin in coins)
+        {
+            rotate(coin);
+        }
+    }
+    void rotate(GameObject coin)
+    {
+        coin.transform.Rotate(2, 0, 0);
+    }
+}
