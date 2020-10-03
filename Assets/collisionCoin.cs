f173a03a10cd194a8cfa95989f3c48b3b345a66c
Jaishree Bala <jaishree1bala@gmail.com>
2020-10-03 01:06:50 -0400

    First stage completed

---
+++ Assets/collisionCoin.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class collisionCoin : MonoBehaviour
+{
+    public int score;
+    public Text txt;
+
+    public GameObject[] stage;
+    public string scoreString;
+    public Material WinningStage;
+
+    void Start()
+    {
+        score = GameObject.FindGameObjectsWithTag("Coin").Length;
+        stage = GameObject.FindGameObjectsWithTag("Stage");
+        Debug.Log(score);
+        scoreString = "Coins Remaining: " + score.ToString();
+        txt.GetComponent<UnityEngine.UI.Text>().text = scoreString;
+
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Coin")
+        {
+            Debug.Log("meh jeezri sad");
+            Debug.Log(collisionInfo.collider);
+            collisionInfo.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            Destroy(collisionInfo.collider);
+            score--;
+            Debug.Log(score);
+
+            if (score > 0)
+            {
+                scoreString = "Coins Remaining: " + score.ToString();
+
+            }
+            else
+            {
+                scoreString = "All Coins Collected";
+                foreach (GameObject i in stage)
+                {
+                    i.GetComponent<MeshRenderer>().material = WinningStage;
+                }
+
+            }
+            txt.GetComponent<UnityEngine.UI.Text>().text = scoreString;
+
+            // change score
+        }
+        if (collisionInfo.collider.tag == "Stage")
+        {
+            if (score > 0)
+            {
+                Debug.Log("YA DEAD");
+            }
+            else
+            {
+                if (collisionInfo.collider.name == "podium")
+                {
+                    Debug.Log("You win :pppppp");
+                    // collisionInfo.gameObject.GetComponent<MeshRenderer>().material = WinningStage;
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float force = 2000f;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(-force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, force * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(0, 0, -force * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
